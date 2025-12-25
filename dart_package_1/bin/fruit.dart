95294f349f431cf63c44c86aed30f272707dcf93
Mugunthan <smugunthan790@gmail.com>
2025-12-25 12:22:33 +0530

    Add initial implementation of fruit price calculation in fruit.dart

---
+++ dart_package_1/bin/fruit.dart
@@ -0,0 +1,9 @@
+void main(){
+  String fruit="Apple";
+  int price=10;
+  int count=2;
+  int total=count*price;
+  print(fruit);
+  print("count of "+fruit+":"+count.toString());
+  print("total price :"+total.toString());
+}
