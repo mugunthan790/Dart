0b808c0edf783768a1deb873bb3bcdd206f6c28f
Mugunthan <smugunthan790@gmail.com>
2025-12-26 11:33:47 +0530

    using function in code

---
+++ dart_package_1/bin/phone.dart
@@ -0,0 +1,14 @@
+void apple(){
+  print("This is apple");
+}
+void samsung(){
+  print("This is samsung");
+}
+void redmi(){
+ print("This is redmi");
+}
+void main(){
+  apple();
+  redmi();
+  samsung();
+}
